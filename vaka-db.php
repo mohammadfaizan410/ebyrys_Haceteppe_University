2af485dc938c2bb0f32ebb31ab85aefba174f928
AtaTasyapar <mert.tasyapar@ug.bilkent.edu.tr>
2023-05-04 21:36:53 +0300

    testt

--- vaka-db.php
+++ vaka-db.php
@@ -6,7 +6,7 @@ if (isset($_POST)) {
     echo 'aasdafa';
     $pdffile = $_FILES['file'];
     $saveto = __DIR__ . DIRECTORY_SEPARATOR . 'vakalar' . DIRECTORY_SEPARATOR . time() . '_' . $pdffile['name'];
-
+    echo $saveto;
     //Move file:
     move_uploaded_file($pdffile['tmp_name'], $saveto);
     echo 'mmmmmmmmmmmm';
