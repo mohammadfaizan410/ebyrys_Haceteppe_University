7fffdd14cee8e64b8c91478547b8da1ef936e40b
AtaTasyapar <mert.tasyapar@ug.bilkent.edu.tr>
2023-05-07 18:22:08 +0300

    Revert "routing db"
    This reverts commit 70b7bad9c1293b051726a1793e0508331f1a4a67.

--- config-teachers.php
+++ config-teachers.php
@@ -1,7 +1,7 @@
 <?php
 $db_user = "admin";
 $db_pass = "12345678";
-$db_name = "ebyrys_db";
-$db_host = "18.159.134.238";
-$db = new PDO('mysql:host=' . $db_host .';dbname=' . $db_name . ';charset-utf8', $db_user, $db_pass);
+$db_name = "e-byrys_db";
+$db_host ="ebyrys.cpcbxswdfvjg.eu-central-1.rds.amazonaws.com";
+$db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset-utf8', $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
