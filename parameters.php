3f176b452166fc9fc91fa8e9db9f2b142a7049a5
AtaTasyapar <mert.tasyapar@ug.bilkent.edu.tr>
2023-05-07 18:36:05 +0300

    update

--- parameters.php
+++ parameters.php
@@ -346,7 +346,7 @@ foreach ($values as $value) {
                                     '> <button type='button' id = '" . $value['patient_id'] . "' class='btn btn-success'>Detay</button> </td>
                                     <td style='
                                     color: black; font-size: 18px;
-                                    '> <a class='nav-items' id ='openform" . $value['patient_id'] . "' href='./openForm.php/?patient_id=" . $value['patient_id'] . "&notlar=" . $value['notlar'] . "&uyaran=" . $value['uyaran'] . "&nemlilik=" . $value['nemlilik'] . "&aktivite=" . $value['aktivite'] . "&hareket=" . $value['hareket'] . "&beslenme=" . $value['beslenme'] . "&surtunme=" . $value['surtunme'] . "&fileid=" . $value['fileid'] . "' class='btn btn-success'>Update</a> </td>
+                                    '> <a class='nav-items' id ='openform" . $value['patient_id'] . "' href='openForm.php/?patient_id=" . $value['patient_id'] . "&notlar=" . $value['notlar'] . "&uyaran=" . $value['uyaran'] . "&nemlilik=" . $value['nemlilik'] . "&aktivite=" . $value['aktivite'] . "&hareket=" . $value['hareket'] . "&beslenme=" . $value['beslenme'] . "&surtunme=" . $value['surtunme'] . "&fileid=" . $value['fileid'] . "' class='btn btn-success'>Update</a> </td>
                                     <td style='
                                     color: black; font-size: 18px;
                                     '> <button class='btn btn-success' id='delete-patient' value='" . $value['patient_id'] . "'>Delete</button> </td></tr>
