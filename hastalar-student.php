394418ccb9b6dc0ab2817d96478c50c6e63742aa
AtaTasyapar <mert.tasyapar@ug.bilkent.edu.tr>
2023-05-07 20:24:46 +0300

    small-fix

--- hastalar-student.php
+++ hastalar-student.php
@@ -99,10 +99,10 @@ if (isset($_GET['logout'])) {
                         <tbody>
                             <?php
 
-                        // require('parameters.php');
-                        // // require('parameters.php');
-                        foreach ($values as $key => $value) {
-                            echo "<tr>
+                            // require('parameters.php');
+                            // // require('parameters.php');
+                            foreach ($values as $key => $value) {
+                                echo "<tr>
                             
                             <td style='
                             color: black; font-size: 18px;
@@ -119,19 +119,19 @@ if (isset($_GET['logout'])) {
                             <td style='
                             color: black; font-size: 18px;
                             '>
-                             <a class='nav-items' id ='openform" . $value['patient_id'] . "' href='./showParameters.php/?patient_id=" . $value['patient_id'] . "&notlar=" . $value['notlar'] . "&uyaran=" . $value['uyaran'] . "&nemlilik=" . $value['nemlilik'] . "&aktivite=" . $value['aktivite'] . "&hareket=" . $value['hareket'] . "&beslenme=" . $value['beslenme'] . "&surtunme=" . $value['surtunme'] . "&fileid=" . $value['fileid'] . "' class='btn btn-success'>Detay</a> </td>
+                             <a class='btn btn-success' id ='openform" . $value['patient_id'] . "' href='./showParameters.php/?patient_id=" . $value['patient_id'] . "&notlar=" . $value['notlar'] . "&uyaran=" . $value['uyaran'] . "&nemlilik=" . $value['nemlilik'] . "&aktivite=" . $value['aktivite'] . "&hareket=" . $value['hareket'] . "&beslenme=" . $value['beslenme'] . "&surtunme=" . $value['surtunme'] . "&fileid=" . $value['fileid'] . "' class='btn btn-success'>Detay</a> </td>
                             <td style='
                             color: black; font-size: 18px;
                             '> 
-                             <a class='nav-items' id ='showParams" . $value['patient_id'] . "' href='./openForm.php/?patient_id=" . $value['patient_id'] . "&notlar=" . $value['notlar'] . "&uyaran=" . $value['uyaran'] . "&nemlilik=" . $value['nemlilik'] . "&aktivite=" . $value['aktivite'] . "&hareket=" . $value['hareket'] . "&beslenme=" . $value['beslenme'] . "&surtunme=" . $value['surtunme'] . "&fileid=" . $value['fileid'] . "' class='btn btn-success'>Update</a> </td>
+                             <a class='btn btn-success' id ='showParams" . $value['patient_id'] . "' href='./openForm.php/?patient_id=" . $value['patient_id'] . "&notlar=" . $value['notlar'] . "&uyaran=" . $value['uyaran'] . "&nemlilik=" . $value['nemlilik'] . "&aktivite=" . $value['aktivite'] . "&hareket=" . $value['hareket'] . "&beslenme=" . $value['beslenme'] . "&surtunme=" . $value['surtunme'] . "&fileid=" . $value['fileid'] . "' class='btn btn-success'>Update</a> </td>
                             <td style='
                             color: black; font-size: 18px;
                             '> 
                             <button class='btn btn-success' id='delete-patient' value='" . $value['patient_id'] . "'>Delete</button> </td></tr>
                             ";
-                        }
-                        
-                        ?>
+                            }
+
+                            ?>
 
                         </tbody>
                     </table>
@@ -139,63 +139,63 @@ if (isset($_GET['logout'])) {
             </div>
         </div>
         <script>
-        $("#formCloser").css('display', 'none');
-        $("#closeOpenForm").css('display', 'none');
-        $(function() {
-            $.ajaxSetup({
-                cache: false
-            }); // disable caching for all requests.
-
-            // RAW Text/Html data from a file
-            $(function() {
-                $("a.nav-items").on("click", function(e) {
-                    e.preventDefault();
-                    $(".send-patient").css('display', 'none');
-                    $("#formCloser").css('display', 'block');
-                    $("#closeOpenForm").css('display', 'block');
-                    $('#contentContainer').load(this.href);
-                })
-            })
-
-        });
-
-        $(function() {
-            const deleteButtons = document.querySelectorAll('#delete-patient');
-            deleteButtons.forEach(button => {
-                button.addEventListener('click', function(e) {
-                    var patient_id = e.target.value;
-                    e.preventDefault();
-                    $.ajax({
-                        type: "POST",
-                        url: "./deletePatient.php",
-                        data: {
-                            patient_id: patient_id
-                        },
-                        success: function(response) {
-                            alert("successfully deleted patient")
-                            location.reload()
-                        },
-                        failure: function(response) {
-                            console.log(response)
-                            alert("error");
-                        }
-
-                    });
-                })
-            })
-        })
-
-        $('#formCloser').click(function(e) {
-            e.preventDefault();
-            $(".send-patient").css('display', 'block');
             $("#formCloser").css('display', 'none');
-        });
+            $("#closeOpenForm").css('display', 'none');
+            $(function() {
+                $.ajaxSetup({
+                    cache: false
+                }); // disable caching for all requests.
+
+                // RAW Text/Html data from a file
+                $(function() {
+                    $("a.nav-items").on("click", function(e) {
+                        e.preventDefault();
+                        $(".send-patient").css('display', 'none');
+                        $("#formCloser").css('display', 'block');
+                        $("#closeOpenForm").css('display', 'block');
+                        $('#contentContainer').load(this.href);
+                    })
+                })
+
+            });
+
+            $(function() {
+                const deleteButtons = document.querySelectorAll('#delete-patient');
+                deleteButtons.forEach(button => {
+                    button.addEventListener('click', function(e) {
+                        var patient_id = e.target.value;
+                        e.preventDefault();
+                        $.ajax({
+                            type: "POST",
+                            url: "./deletePatient.php",
+                            data: {
+                                patient_id: patient_id
+                            },
+                            success: function(response) {
+                                alert("successfully deleted patient")
+                                location.reload()
+                            },
+                            failure: function(response) {
+                                console.log(response)
+                                alert("error");
+                            }
+
+                        });
+                    })
+                })
+            })
+
+            $('#formCloser').click(function(e) {
+                e.preventDefault();
+                $(".send-patient").css('display', 'block');
+                $("#formCloser").css('display', 'none');
+            });
         </script>
         <script>
-        $(window).on('load', function() {
-            $("body").removeClass("preload");
-            $("#openFormContainer").removeClass("preload");
-        });
+            $(window).on('load', function() {
+                $("body").removeClass("preload");
+                $("#openFormContainer").removeClass("preload");
+            });
         </script>
         <!-- JavaScript Libraries -->
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
