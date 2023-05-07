5eb67389f6df9ada924128cea2e40015bffb1059
AtaTasyapar <mert.tasyapar@ug.bilkent.edu.tr>
2023-05-07 20:07:51 +0300

    fix

--- config-teachers.php
+++ config-teachers.php
@@ -2,6 +2,6 @@
 $db_user = "admin";
 $db_pass = "12345678";
 $db_name = "ebyrys_db";
-$db_host = '"ebyrys.cpcbxswdfvjg.eu-central-1.rds.amazonaws.com';
+$db_host = "ebyrys.cpcbxswdfvjg.eu-central-1.rds.amazonaws.com";
 $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset-utf8', $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
