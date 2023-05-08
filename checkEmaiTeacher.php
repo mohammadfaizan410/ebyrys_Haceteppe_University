2edec9775ea99029b4e5294185b05c75bcb177b6
Muhammad Faizan <mohammadfaizan410@gmail.com>
2023-05-08 19:53:31 +0300

    sql-update

--- checkEmaiTeacher.php
+++ checkEmaiTeacher.php
@@ -1,18 +1,18 @@
 <?php
 require_once("config-students.php");
-?>
-<?php
-if (isset($_POST)) {
 
+if (isset($_POST['email'])) {
     $email = $_POST['email'];
-    
-    $sql = "Select * from teachers where email = '$email'";
-    $result = mysqli_query($conn, $sql);
-    $present= mysqli_num_rows($result);
-    if ($present > 0) {
+    $sql = "SELECT * FROM teachers WHERE email = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->execute([$email]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($result) {
         echo 'exists';
     } else {
         echo 'not-exists';
     }
-} else
+} else {
     echo 'no data';
+}
+?>
